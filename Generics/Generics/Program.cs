2c94aedca5d5ac1412caaeb799f35aec183fbc83
Timani G <timanig@gmail.com>
2019-11-06 17:00:54 -0500

    /Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nonGenericCollection = new ArrayList();
+            nonGenericCollection.Add(4);
+            nonGenericCollection.Add(4);
+            nonGenericCollection.Add(4);
+            nonGenericCollection.Add("str");
+
+            Console.WriteLine("Print ArrayList");
+            foreach (var x in nonGenericCollection)
+            {
+                Console.WriteLine(x);
+            }
+
+            //var GenericCollection = new List<int>();
+            //GenericCollection.Add(4);
+            //GenericCollection.Add(4);
+            //GenericCollection.Add(4);
+            //GenericCollection.Add("str");
+
+            //Console.WriteLine("Print ArrayList");
+            //foreach (var x in nonGenericCollection)
+            //{
+            //    Console.WriteLine(x);
+            //}
+        }
+    }
+}
